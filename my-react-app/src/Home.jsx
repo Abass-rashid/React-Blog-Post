9b445cfb97b62c089e1f7c4182bd34f1181a32b6
Abazrashid <abazrashid144@gmail.com>
2023-09-23 10:46:11 +0300

    Add click event

--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log("Hello, Net Ninja!");
+  }
+  const handleClickAgain = (name,event) => {
+    console.log(`Hello ${name} how are you! ${event.target}`)
+  }
+
   return ( 
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={ ()=> {handleClickAgain("Jafar",event)}}>Click me Again</button>
     </div>
    );
 }
